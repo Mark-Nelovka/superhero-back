a666dfb71c73ec9fa6221fdacc32be5ec9ed8b4e
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-05-29 01:12:59 +0300

    add command for build

--- index.js
+++ index.js
@@ -21,4 +21,10 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 
+app.listen(process.env.PORT || 8080, function () {
+  console.log(
+    `CORS-enabled web server listening on port ${process.env.PORT || 8080}`
+  );
+});
+
 export default app;
--- server.js
+++
@@ -1,7 +0,0 @@
-import app from "./index.js";
-
-app.listen(process.env.PORT || 8080, function () {
-  console.log(
-    `CORS-enabled web server listening on port ${process.env.PORT || 8080}`
-  );
-});
