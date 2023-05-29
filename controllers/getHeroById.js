18a54d3042e85638d13da86e1b18ce8f9560ec1f
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-05-29 07:08:25 +0300

    change setting for BD

--- controllers/getHeroById.js
+++ controllers/getHeroById.js
@@ -19,27 +19,26 @@ const pool = new Pool({
 
 async function getHeroById(req, res) {
   const { id } = req.params;
-  console.log("HEROID: ", id);
-  // try {
-  //   const singleHero = await pool.query(
-  //     `select * from superhero where hero_id='${id}'`
-  //   );
-  //   res.status(200).json({
-  //     code: 200,
-  //     status: "success",
-  //     message: "Hero was loading",
-  //     data: JSON.stringify(singleHero.rows),
-  //   });
-  // } catch (error) {
-  //   res.status(500).json({
-  //     code: 500,
-  //     status: "error",
-  //     message: "Something went wrong",
-  //     data: {
-  //       error,
-  //     },
-  //   });
-  // }
+  try {
+    const singleHero = await pool.query(
+      `select * from superhero where hero_id='${id}'`
+    );
+    res.status(200).json({
+      code: 200,
+      status: "success",
+      message: "Hero was loading",
+      data: JSON.stringify(singleHero.rows),
+    });
+  } catch (error) {
+    res.status(500).json({
+      code: 500,
+      status: "error",
+      message: "Something went wrong",
+      data: {
+        error,
+      },
+    });
+  }
 }
 
 export default getHeroById;
